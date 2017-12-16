8a699219089fa9e6ddc9190e21142a64e6ddb9d5
Marc-Antoine Dumais <marc-antoine.dumais@mail.mcgill.ca>
2017-12-16 00:36:49 -0500

    working solver for 3x3 and 4x4

--- main.go
+++ main.go
@@ -17,8 +17,11 @@ func main() {
 
     g := loadGrid(os.Args[1])
     fmt.Println(g.String())
-    g.solve()
-    fmt.Println(g.String())
+    if g.solve() {
+        fmt.Println(g.String())
+    } else {
+        fmt.Println("Could not find a solution for this sudoku.")
+    }    
 }
 
 func check(e error) {
@@ -33,8 +36,8 @@ func timeTrack(start time.Time, name string) {
 }
 
 type grid struct {
-    n uint8
-    g [][]uint8
+    n int
+    g [][]int
 }
 
 func loadGrid(filename string) grid {
@@ -48,9 +51,9 @@ func loadGrid(filename string) grid {
     n, _ := strconv.Atoi(lines[0])
     lines = lines[2:]
 
-    g := make([][]uint8, n*n)
+    g := make([][]int, n*n)
     for i := range g {
-        g[i] = make([]uint8, n*n)
+        g[i] = make([]int, n*n)
     }
 
     realRow := 0
@@ -66,22 +69,92 @@ func loadGrid(filename string) grid {
                 continue
             }
             if line[j] == "x" {
-                g[realRow][realCol] = uint8(0)
+                g[realRow][realCol] = 0
             } else {
-                tmp, _ := strconv.Atoi(line[j])
-                g[realRow][realCol] = uint8(tmp)
+                g[realRow][realCol], _ = strconv.Atoi(line[j])
             }
             realCol++
         }
         realRow++
     }
-    return grid{uint8(n), g}
+    return grid{n, g}
 }
 
-func (gr *grid) solve() {
+func (gr *grid) solve() bool {
     defer timeTrack(time.Now(), "Solver")
 
     //Solve the sudoku
+    rows := make([]uint64, gr.n*gr.n)
+    columns := make([]uint64, gr.n*gr.n)
+    blocks := make([]uint64, gr.n*gr.n)
+    
+    for i := range gr.g {
+        for j := range gr.g[i] {
+            val := gr.g[i][j]
+            if val != 0 {
+                rows[i] |= 1 << uint(val-1)
+                columns[j] |= 1 << uint(val-1)
+                blocks[i/gr.n * gr.n + j/gr.n] |= 1 << uint(val-1)
+            }
+        }
+    }
+    
+    var recurse func(i, j int) bool
+    recurse = func(i, j int) bool {
+        //fmt.Println(i, j)
+        for ; i < len(gr.g); i++ {
+            for ; j < len(gr.g[i]); j++ {
+                blockI := i/gr.n * gr.n + j/gr.n
+                curRow := rows[i]
+                curCol := columns[j]
+                curBlock := blocks[blockI]
+                
+                if gr.g[i][j] != 0 {
+                    continue
+                }
+                for v := 0; v < gr.n*gr.n; v++ {
+                    if (curRow & (1 << uint(v))) != 0 ||
+                       (curCol & (1 << uint(v))) != 0 ||
+                       (curBlock & (1 << uint(v))) != 0 {
+                       continue
+                    }
+                    //fmt.Println("v ", v)
+                    gr.g[i][j] = v + 1
+                    rows[i] |= 1 << uint(v)
+                    columns[j] |= 1 << uint(v)
+                    blocks[blockI] |= 1 << uint(v)
+                    
+                    if recurse(i, j+1) {
+                        return true
+                    } else {
+                        rows[i] = curRow
+                        columns[j] = curCol
+                        blocks[blockI] = curBlock
+                        gr.g[i][j] = 0
+                    }
+                }
+                return false
+            }
+            j = 0
+        }
+        return true
+    }
+    
+    return recurse(0, 0)
+    
+    /*fmt.Println("rows")
+    for i, k := range rows {
+        fmt.Printf("%d: %s\n", i, strconv.FormatInt(int64(k), 2))
+    }
+    fmt.Println("columns")
+    for i, k := range columns {
+        fmt.Printf("%d: %s\n", i, strconv.FormatInt(int64(k), 2))
+    }
+    fmt.Println("blocks")
+    for i, k := range blocks {
+        fmt.Printf("%d: %s\n", i, strconv.FormatInt(int64(k), 2))
+    }
+    */
 }
 
 func (gr *grid) String() string {
@@ -96,13 +169,13 @@ func (gr *grid) String() string {
                 s += " "
             }
             s += fmt.Sprintf("%d ", v)
-            if j%int(gr.n) == int(gr.n-1) && j != len(row)-1 {
+            if j%gr.n == gr.n-1 && j != len(row)-1 {
                 s += "| "
             }
         }
-        if i%int(gr.n) == int(gr.n-1) && i != len(gr.g)-1 {
+        if i%gr.n == gr.n-1 && i != len(gr.g)-1 {
             s += "\n"
-            for j := 0; j < int(gr.n*gr.n)*(digitSize+1) + (int(gr.n)-1)*2 - 1; j++ {
+            for j := 0; j < gr.n*gr.n*(digitSize+1) + (gr.n-1)*2 - 1; j++ {
                 s += "-"
             }
         }
